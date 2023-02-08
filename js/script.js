e406ede83da428859cfb62a0670b020c1d8213d6
Artem <goldtree3000@gmail.com>
2023-02-09 02:02:44 +0300

    1

--- js/script.js
+++ js/script.js
@@ -13,12 +13,10 @@ if (iconMenu) {
 //<IBG>
 
 function ibg() {
-	if (isIE()) {
-		let ibg = document.querySelectorAll(".ibg");
-		for (var i = 0; i < ibg.length; i++) {
-			if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
-				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-			}
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
 		}
 	}
 }
