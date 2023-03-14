5376fffbad9ea035f9fa763229d063a3ed48191a
Art <106353035+ArtemMikhalenia@users.noreply.github.com>
2023-03-14 21:55:23 +0300

    scroll-fix

--- js/script.js
+++ js/script.js
@@ -46,18 +46,6 @@ placeholder.forEach(el =>
 		emailInputContact.placeholder = placeholder.value = "Введите свой email";
 	}));
 
-//<IBG>
-
-// function ibg() {
-// 	let ibg = document.querySelectorAll(".ibg");
-// 	for (var i = 0; i < ibg.length; i++) {
-// 		if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
-// 			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-// 		}
-// 	}
-// }
-// ibg();
-
 
 //POPUP
 const popupLinks = document.querySelectorAll('.popup-link');
